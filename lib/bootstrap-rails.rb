0c475ff3e68e461353a6c22a3b63a41a107438c0
Fabio Stapait <stapait@gmail.com>
2012-01-31 20:50:31 -0200

    Adding missing file from previous commit

--- lib/bootstrap-rails.rb
+++ lib/bootstrap-rails.rb
@@ -1,10 +1,12 @@
 require "bootstrap-rails/version"
 
 module BootstrapRails
-  class Railtie < Rails::Railtie
-    config.app_generators do |g|
-      # Make templates loadable from gem insted of copying to app
-      g.templates.unshift File::expand_path('../templates', __FILE__)
-    end 
+  module Rails
+    class Engine < ::Rails::Engine
+      config.app_generators do |g|
+        # Make templates loadable from gem insted of copying to app
+        g.templates.unshift File::expand_path('../templates', __FILE__)
+      end
+    end
   end
 end
